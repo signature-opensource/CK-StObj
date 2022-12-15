c288f07ddf1dd7418ac8b6c6375d01a217209ffa
Olivier Spinelli <olivier.spinelli@signature-code.com>
2022-12-15 16:43:51 +0100

    Restored commented roundtrip tests.

--- Tests/CK.Poco.Exc.Json.Tests/RecordTests.cs
+++ Tests/CK.Poco.Exc.Json.Tests/RecordTests.cs
@@ -1,4 +1,5 @@
 using CK.Core;
+using CK.Setup;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
@@ -35,8 +36,8 @@ namespace CK.Poco.Exc.Json.Tests
             } );
             o.ToString().Should().Be( @"{""Hop"":{""Name"":""Albert"",""Count"":3712}}" );
 
-            //var o2 = JsonTestHelper.Roundtrip( directory, o );
-            //o2.Hop.Should().Be( ("CodeGen!", 3712) );
+            var o2 = JsonTestHelper.Roundtrip( directory, o );
+            o2.Hop.Should().Be( new Thing( "Albert", 3712 ) );
         }
 
         public interface IWithNullableRecord : IPoco
@@ -55,15 +56,39 @@ namespace CK.Poco.Exc.Json.Tests
             var o = f.Create( o => { o.Hop = new Thing("Hip", 42); } );
             o.ToString().Should().Be( @"{""Hop"":{""Name"":""Hip"",""Count"":42}}" );
 
-            //var o2 = JsonTestHelper.Roundtrip( directory, o );
-            //o2.Hop.Should().Be( ("CodeGen!", 3712) );
+            var o2 = JsonTestHelper.Roundtrip( directory, o );
+            o2.Hop.Should().Be( new Thing( "Hip", 42 ) );
 
             o.Hop = null;
             o.ToString().Should().Be( @"{""Hop"":null}" );
 
-            //var o3 = JsonTestHelper.Roundtrip( directory, o );
-            //o3.Hop.Should().BeNull();
+            var o3 = JsonTestHelper.Roundtrip( directory, o );
+            o3.Hop.Should().BeNull();
         }
 
+        public interface IHoldRecList : IPoco
+        {
+            public record struct Rec( IList<Rec> R, int A = 3712 );
+
+            ref Rec P { get; }
+        }
+
+        [Test]
+        public void recursive_list_use_of_named_record_is_handled()
+        {
+            var c = TestHelper.CreateStObjCollector( typeof( PocoJsonExportSupport ), typeof( IHoldRecList ) );
+            using var s = TestHelper.CreateAutomaticServices( c ).Services;
+            var directory = s.GetRequiredService<PocoDirectory>();
+
+            var p = directory.Create<IHoldRecList>();
+            p.P.R.Should().NotBeNull();
+            p.P.A.Should().Be( 3712 );
+            p.P.R.Add( new IHoldRecList.Rec( new List<IHoldRecList.Rec>(), 42 ) );
+
+            p.ToString().Should().Be( @"{""P"":{""R"":[{""R"":[],""A"":42}],""A"":3712}}" );
+
+            var p2 = JsonTestHelper.Roundtrip( directory, p );
+            p2.Should().BeEquivalentTo( p );
+        }
     }
 }
