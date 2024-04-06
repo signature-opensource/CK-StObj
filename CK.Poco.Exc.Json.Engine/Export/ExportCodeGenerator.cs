5cd899d044d0a7c5e34231360422f30ead86c80c
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-04-06 17:04:02 +0200

    Added WriteJson for IPoco? on PocoDirectory.

--- CK.Poco.Exc.Json.Engine/Export/ExportCodeGenerator.cs
+++ CK.Poco.Exc.Json.Engine/Export/ExportCodeGenerator.cs
@@ -1,5 +1,6 @@
 using CK.CodeGen;
 using CK.Core;
+using System.Runtime.CompilerServices;
 
 namespace CK.Setup.PocoJson
 {
@@ -29,19 +30,30 @@ namespace CK.Setup.PocoJson
         void SupportPocoDirectoryJsonExportGenerated( ITypeScope pocoDirectory )
         {
             pocoDirectory.Definition.BaseTypes.Add( new ExtendedTypeName( "CK.Core.IPocoDirectoryJsonExportGenerated" ) );
-            var read = pocoDirectory
+            var wAny = pocoDirectory
                             .CreateFunction( "void CK.Core.IPocoDirectoryJsonExportGenerated.WriteAnyJson( " +
                                                         "System.Text.Json.Utf8JsonWriter w, " +
                                                         "object? o, " +
                                                         "Poco.Exc.Json.PocoJsonExportOptions? options)" );
 
-            read.GeneratedByComment()
+            wAny.GeneratedByComment()
                 .Append( "if( o == null ) w.WriteNullValue();" ).NewLine()
                 .Append( "else" )
                 .OpenBlock()
-                .Append( "var wCtx = new CK.Poco.Exc.Json.PocoJsonWriteContext( this, options );" ).NewLine()
+                .Append( "using var wCtx = new CK.Poco.Exc.Json.PocoJsonWriteContext( this, options );" ).NewLine()
                 .Append( _exporterType.FullName ).Append( ".WriteAny( w, o, wCtx );" )
                 .CloseBlock();
+
+            var wPoco = pocoDirectory
+                            .CreateFunction( "bool CK.Core.IPocoDirectoryJsonExportGenerated.WriteJson( " +
+                                                        "System.Text.Json.Utf8JsonWriter w, " +
+                                                        "IPoco? o, " +
+                                                        "Poco.Exc.Json.PocoJsonExportOptions? options)" );
+
+            wPoco.GeneratedByComment()
+                .Append( "if( o == null ) { w.WriteNullValue(); return true; }" ).NewLine()
+                .Append( "using var wCtx = new CK.Poco.Exc.Json.PocoJsonWriteContext( this, options );" ).NewLine()
+                .Append( "return System.Runtime.CompilerServices.Unsafe.As<PocoJsonExportSupport.IWriter>( o ).WriteJson( w, wCtx, true );" );
         }
 
     }
--- CK.Poco.Exc.Json/Export/IPocoDirectoryJsonExportGenerated.cs
+++ CK.Poco.Exc.Json/Export/IPocoDirectoryJsonExportGenerated.cs
@@ -15,5 +15,15 @@ namespace CK.Core
         /// <param name="o">The object to write.</param>
         /// <param name="options">The write options.</param>
         void WriteAnyJson( Utf8JsonWriter w, object? o, PocoJsonExportOptions? options = null );
+
+        /// <summary>
+        /// Writes a <see cref="IPoco"/> (that can be null) with its type and returns whether either "null"
+        /// or the Poco has been written: this is false if the <see cref="PocoJsonExportOptions.TypeFilterName"/>
+        /// does not contain the type.
+        /// </summary>
+        /// <param name="w">The writer.</param>
+        /// <param name="o">The object to write.</param>
+        /// <param name="options">The write options.</param>
+        bool WriteJson( Utf8JsonWriter w, IPoco? o, PocoJsonExportOptions? options = null );
     }
 }
