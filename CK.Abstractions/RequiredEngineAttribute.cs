887f29cf7c7a455694fb32881973406381c200c0
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-10-31 16:45:12 +0100

    Doc and optional versionRange for RequiredEngineAttribute.

--- CK.Abstractions/RequiredEngineAttribute.cs
+++ CK.Abstractions/RequiredEngineAttribute.cs
@@ -4,8 +4,8 @@ namespace CK.Setup;
 
 /// <summary>
 /// Assembly attribute that states that this assembly requires a CKomposable Engine to be setup.
-/// This is typically used by PFeatures definers (see <see cref="IsPFeatureDefinerAttribute"/>) to declare one
-/// or more associated engines.
+/// This is typically used by PFeatures definers (see <see cref="IsPFeatureDefinerAttribute"/>) or PFeatures
+/// (see <see cref="IsPFeatureAttribute"/>) assemblies to declare one or more associated engines.
 /// </summary>
 [AttributeUsage( AttributeTargets.Assembly, AllowMultiple = true )]
 public sealed class RequiredEngineAttribute : Attribute
@@ -14,11 +14,16 @@ public sealed class RequiredEngineAttribute : Attribute
     /// Initializes a new required engine dependency attribute.
     /// </summary>
     /// <param name="assemblyName">Name of the CKomposable Engine assembly.</param>
-    /// <param name="versionBound">
+    /// <param name="versionRange">
     /// A NuGet version range (see https://learn.microsoft.com/en-us/nuget/concepts/package-versioning?tabs=semver20sort#version-ranges)
     /// of the Engine to use.
+    /// <list type="bullet">
+    ///     <item>The default <c>null</c> allows any version.</item>
+    ///     <item>To specify a minimal version, simply writes it: "1.2.0-a" is the same as "[1.2.0-a,)" and allows the 1.2.0-a and greater ones.</item>
+    ///     <item>To fix a version, use "[1.2.0-a]" (the inclusive brackets).</item>
+    /// </list>
     /// </param>
-    public RequiredEngineAttribute( string assemblyName, string versionBound )
+    public RequiredEngineAttribute( string assemblyName, string? versionRange = null )
     {
     }
 }
