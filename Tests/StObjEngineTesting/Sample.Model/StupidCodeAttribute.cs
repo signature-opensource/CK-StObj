e9aac2f39fc7d21fb6ade7b00511aa97f8515742
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-07-15 13:41:26 +0200

    Sample requires C# code generation on EngineAttributes.
    For the moment we must use old model.

--- Tests/StObjEngineTesting/Sample.Model/StupidCodeAttribute.cs
+++ Tests/StObjEngineTesting/Sample.Model/StupidCodeAttribute.cs
@@ -1,3 +1,4 @@
+using CK.Core;
 using CK.Setup;
 using System;
 
@@ -7,7 +8,7 @@ namespace Sample.Model;
 /// Stupid attribute that defines the code of a method that must be abstract or virtual.
 /// </summary>
 [AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
-public class StupidCodeAttribute : EngineAttribute
+public class StupidCodeAttribute : ContextBoundDelegationAttribute // Should be EngineAttribute.
 {
     public StupidCodeAttribute( string actualCode )
         : base( "Sample.Engine.StupidCodeAttributeImpl, Sample.Engine" )
