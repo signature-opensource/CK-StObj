f4c3f97d2d96789b47cefdec891801e89569e5c4
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-09-08 13:53:27 +0200

    Workaround the stupid https://learn.microsoft.com/en-us/dotnet/csharp/misc/cs0718

--- CK.Poco.Exc.Json/CommonPocoJsonSupport.cs
+++ CK.Poco.Exc.Json/CommonPocoJsonSupport.cs
@@ -5,8 +5,12 @@ namespace CK.Core;
 /// <summary>
 /// Triggers the common implementation of Json import and export code generation.
 /// </summary>
+/// <remarks>
+/// This class should be static but because of the stupid https://learn.microsoft.com/en-us/dotnet/csharp/misc/cs0718
+/// it is not so it can be used as a generic argument.
+/// </remarks>
 [ContextBoundDelegation( "CK.Setup.PocoJson.CommonImpl, CK.Poco.Exc.Json.Engine" )]
 [AlsoRegisterType<PocoExchangeService>]
-public static class CommonPocoJsonSupport
+public sealed class CommonPocoJsonSupport
 {
 }
